75a9e06733536096cfde1224a85057d607029192
Kenneth Garcia <kennethgarcia12@hotmail.com>
2019-08-11 23:06:08 -0700

    crated the questions array in main.js

---
+++ resources/js/main.js
@@ -0,0 +1,7 @@
+//questions array
+const questions = [
+    {question: 'Enter Your First Name'},
+    {question: 'Enter Your Last Name'},
+    {question: 'Enter Your Email', pattern: /^\S+@\S+\.\S+$/ },
+    {question: 'Create A Password', type: 'password'}
+];
