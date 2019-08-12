ce537f8df7f41e449148228470b0c9958ca164b4
Kenneth Garcia <kennethgarcia12@hotmail.com>
2019-08-11 23:17:30 -0700

    initialized all constants and selected all DOM elements needed in main.js

--- resources/js/main.js
+++ resources/js/main.js
@@ -5,3 +5,21 @@ const questions = [
     {question: 'Enter Your Email', pattern: /^\S+@\S+\.\S+$/ },
     {question: 'Create A Password', type: 'password'}
 ];
+
+//transition times
+const shakeTime = 100; //shake transition time
+const switchTime = 200; //Transition between questions
+
+//init Position of questions
+let position = 0;
+
+//init DOM Elements
+const formBox = document.querySelector('#form-box');
+const nextBtn = document.querySelector('#next-btn');
+const prevBtn = document.querySelector('#prev-btn');
+const inputGroup = document.querySelector('#input-group');
+const inputField = document.querySelector('#input-field');
+const inputLabel = document.querySelector('#input-label');
+const inputProgress = document.querySelector('#input-progress');
+const progressBar = document.querySelector('#progress-bar');
+
